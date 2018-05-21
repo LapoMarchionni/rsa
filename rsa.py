b6f21bcbbd7b960e905fc4ecd581e7865819d0af
Lapo Marchionni <marchionni.lapo@gmail.com>
2018-05-21 23:03:29 +0200

    CRT

--- rsa.py
+++ rsa.py
@@ -3,10 +3,12 @@ import time
 
 
 class RSA:
-    """RSA Scheme
+    """RSA Scheme.
+    Encrypt using as default the public key and decrypt using as default
+    the private key.
 
     Args:
-        prime_lenght: prime lenght used for the keys generations. 
+        prime_lenght: prime lenght used for the keys generations.
             [default: 10^100]
     """
 
@@ -14,26 +16,33 @@ class RSA:
         self.pl = prime_lenght
         self.pari_keys_generator()
 
-    # Greates Common Divisor
     @staticmethod
     def _gcd(a, b):
-        assert a >= 0 and b >= 0, (
-            "Both number must be positive integers")
-        if b == 0:
-            return b
-        return RSA._gcd(b, a % b)
+        """ Calculate the GCD between two numbers."""
+        while b != 0:
+            a, b = b, a % b
+        return a
 
-    # Extended Euclidian Algorithm
     @staticmethod
     def _egcd(a, b):
+        """Apply the Extended Euclidian Algorithm to two numbers."""
         if a == 0:
             return (b, 0, 1)
         g, y, x = RSA._egcd(b % a, a)
         return (g, x - (b // a) * y, y)
 
-    # Miller-Rabin Primality Test
+    @staticmethod
+    def _modinv(b, n):
+        """Uses the Extended Euclidian Algorithm to calculate the value
+        of b^-1 mod n.
+        """
+        g, x, _ = RSA._egcd(b, n)
+        if g == 1:
+            return x % n
+
     @staticmethod
     def _is_prime(n, iterations=40):
+        """Tes of Miller-Rabin to determinate if a number is prime."""
         s, m = 0, n - 1
         if n in [1, 2]:
             return True
@@ -54,31 +63,114 @@ class RSA:
             return False
         return True
 
-    # Prime number generator
     @staticmethod
     def _prime_generator(k):
+        """Generate a prime number of length k."""
+        def set_bit(v, i, bit):
+            """Change the i-th bit of v with 1 or 0"""
+            mask = 1 << i
+            v &= ~mask
+            if bit == 1:
+                v |= mask
+            return v
+
+        if k == 1:
+            return random.choice([1, 2, 3, 5, 7])
         while True:
-            n = random.randrange(10**(k - 1), 10**k - 1, 1)
-            if RSA._is_prime(n):
-                return n
+            b = random.getrandbits(334)
+            b = set_bit(b, len(str(b))-1, 1)
+            b = set_bit(b, 1, 1)
+            if RSA._is_prime(b):
+                return b
+
+    @staticmethod
+    def print_text(text):
+        """Join element of a list and cast them to string."""
+        return ''.join(str(t) for t in text)
 
     def pari_keys_generator(self):
-        s = time.time()
-        p = RSA._prime_generator(self.pl)
-        print('1 - %s' % (time.time() - s))
-        q = RSA._prime_generator(self.pl)
-        print('2 - %s' % (time.time() - s))
-        n = p * q
-        print('3 - %s' % (time.time() - s))
+        """Generate the private and public keys.
+
+        Variables:
+            - p and q are two prime numbers.
+            - n is their product p*q.
+            - phi is their totient (p-1) * (q-1).
+            - e is a random integer between 1 and phi, coprime with the latter.
+            - d is the modular inverse of e and phi.
+        Returns:
+            Set the private key as (d, n) and the public key as (e, n)
+        """
+        self.__p = RSA._prime_generator(self.pl)
+        self.__q = RSA._prime_generator(self.pl)
+        n = self.__p * self.__q
         # phi totient of n
-        phi = (p - 1) * (q - 1)
-        print('4 - %s' % (time.time() - s))
+        phi = (self.__p - 1) * (self.__q - 1)
         # find an e coprime with phi
         e = random.randrange(1, phi)
         while RSA._gcd(e, phi) != 1:
             e = random.randrange(1, phi)
-        print('5 - %s' % (time.time() - s))
-        d = RSA._egcd(e, phi)[0]
-        print('6 - %s' % time.time() - s)
-        self.pub_key = (e, n)
-        self.priv_key = (d, n)
+        # d = e^-1 mod phi
+        d = RSA._modinv(e, phi)
+        self.__pub_key = (e, n)
+        self.__priv_key = (d, n)
+
+    def encrypt(self, plaintext, key='pub'):
+        """Encrypt a text by calculating the value of m^k mod n 
+        for every letter m in the plaintext.
+        k is equal to e if the public key is being used, else to d.
+
+        Args:
+            plaintext: the text to be encrypted.
+            key: 'pub' for public key or 'priv' for private key.
+                [default: 'pub']
+        Return:
+            A list of every letter encrypted.
+        """
+        if key == 'pub':
+            k, n = self.__pub_key
+        else:
+            k, n = self.__priv_key
+        ct = [pow(ord(m), k, n) for m in plaintext]
+        return ct
+
+    def decrypt(self, ciphertext, key='priv'):
+        """Decript a list of encrypted letters using c^k mod n
+        for every number c in the list.
+        k is equal to e if the public key is being used, else to d.
+
+        Args:
+            ciphertext: the list of numbers to be decrypted.
+            key: 'pub' for public key or 'priv' for private key.
+                [default: 'priv']
+        Return:
+            A decrypted plaintext.
+        """
+        if key == 'priv':
+            k, n = self.__priv_key
+        else:
+            k, n = self.__pub_key
+        pt = [chr(pow(c, k, n)) for c in ciphertext]
+        return RSA.print_text(pt)
+
+    def crt_decrypt(self, ciphertext, key='priv'):
+        """ RSA decryption using the Chinese Remainder Theorem. """
+        if key == 'priv':
+            k, n = self.__priv_key
+        else:
+            k, n = self.__pub_key
+        dP = RSA._modinv(k, self.__p - 1)
+        dQ = RSA._modinv(k, self.__q - 1)
+        coeff = RSA._modinv(self.__q, self.__p)
+        print(self.__p)
+        print(self.__q)
+        print(dP)
+        print(dQ)
+        print(coeff)
+        pt = []
+        for c in ciphertext:
+            m1 = pow(c, dP, self.__p)
+            m2 = pow(c, dQ, self.__q)
+            h = coeff * (m1 - m2) % self.__p
+            m = m2 + h*self.__q
+            pt.append(chr(m))
+        return RSA.print_text(pt)
